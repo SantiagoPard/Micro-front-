09b30a8d22531bdccd6b88bd1e2236eeca61f743
unknown <lspardo@uniboyaca.edu.com>
2023-06-02 02:16:22 -0500

    se agrego el metodo para eliminar un estudiante

--- js/gestionEstudiantes.js
+++ js/gestionEstudiantes.js
@@ -1,9 +1,9 @@
 let modificarEstudiante = () => { };
-let eliminarUsuario = () => { };
+let eliminarEstudiante = () => { };
 let reload = () => { };
 
 $(document).ready(function () {
-        if(window.location.href == 'http://127.0.0.1:5500/index.html'){
+    if (window.location.href == 'http://127.0.0.1:5500/index.html') {
         $.ajax({
             method: 'get',
             url: 'http://localhost:8000/estudiantes'
@@ -16,12 +16,12 @@ $(document).ready(function () {
             estudiantes.forEach(estudiantes => {
                 html += '<tr>';
                 html += '   <td>' + estudiantes.codigo + '</td>';
-                html += '   <td>' + estudiantes.nombres +' '+ estudiantes.apellidos + '</td>';
+                html += '   <td>' + estudiantes.nombres + ' ' + estudiantes.apellidos + '</td>';
                 html += '<td>';
-                html += ' <button onclick="modificarEstudiante('+ estudiantes.id +')">Modificar</button>';
+                html += ' <button onclick="modificarEstudiante(' + estudiantes.codigo + ')">Modificar</button>';
                 html += '</td>';
                 html += '<td>';
-                html += '<button onclick="eliminarEstudiante('+ estudiantes.id +')">Eliminar</button>';
+                html += '<button onclick="eliminarEstudiante(' + estudiantes.codigo + ')">Eliminar</button>';
                 html += '</td>';
                 html += '</tr>';
             });
@@ -29,9 +29,8 @@ $(document).ready(function () {
         }).fail((error) => {
             console.error(error);
         });
-        }
-    
-    let indexActividadSeleccionada = 0
+    }
+
     let idEstudiante = 0;
 
     /**
@@ -44,91 +43,59 @@ $(document).ready(function () {
         idEstudiante = id;
         window.location.assign('../registrarEstudiante.html');
         $.ajax({
-            url: `http://localhost:8000/estudiantes${id}`,
+            url: 'http://localhost:8000/estudiantes/'+`${id}`,
             method: 'get'
         }).done(response => {
             const dataJson = JSON.parse(response);
             const msg = dataJson.data;
             console.log(msg);
             $('#name').val(msg.codigo);
-            $('#username').val(msg.nombres +''+ msg.apellidos);
+            $('#username').val(msg.nombres + '' + msg.apellidos);
             $('#password').val(msg.apellidos);
         });
 
     }
 
-    /**
-     * Funcianalidad para registrar un usuario
-     */
-    // $('#registrar').click(function () {
-    //     indexActividadSeleccionada = -1
-    //     $codigo = $('#codigo').val("");
-    //     $nombres = $('#nombres').val("");
-    //     $apellidos = $('#apellidos').val("");
-    //     $('#titulo').html("registrar");
-    // })
 
     /**
     * Funcianalidad para guardar un registro o una modificacion
     */
     $('#guardar').click(function () {
-        alert('hoola');
-        if (indexActividadSeleccionada == -1) {
-            alert('hoola');
-            $codigo = $('#codigo').val();
-            $nombres = $('#nombres').val();
-            $apellidos = $('#apellidos').val();
-            $.ajax({
-                url: 'http://localhost:8000/estudiantes',
-                method: 'post',
-                data: {
-                    codigo: $codigo,
-                    nombres: $nombres,
-                    apellidos: $apellidos
-                }
-            }).done(response => {
-                const dataJson = JSON.parse(response);
-                const msg = dataJson.data;
-                alert(msg);
-                
-            });
-        } else if (indexActividadSeleccionada == 1) {
-            $name = $('#name').val();
-            $username = $('#username').val();
-            $password = $('#password').val();
-            $.ajax({
-                url: 'http://localhost:8000/estudiantes' + `/${idEstudiante}`,
-                method: 'put',
-                data: {
-                    name: $name,
-                    username: $username,
-                    password: $password
-                }
-            }).done(response => {
-                const dataJson = JSON.parse(response);
-                const msg = dataJson.data;
-                alert(msg);
-                reload();
-            });
-        } else {
-            alert('Debe seleccionar modificar o registrar');
-        }
+
+        $name = $('#name').val();
+        $username = $('#username').val();
+        $password = $('#password').val();
+        $.ajax({
+            url: 'http://localhost:8000/estudiantes' + `/${idEstudiante}`,
+            method: 'put',
+            data: {
+                name: $name,
+                username: $username,
+                password: $password
+            }
+        }).done(response => {
+            const dataJson = JSON.parse(response);
+            const msg = dataJson.data;
+            alert(msg);
+            reload();
+        });
+
     })
 
     /**
     * Funcianalidad para eliminar un usuario
     */
 
-    eliminarUsuario = function (posicionArray) {
-        let id = posicionArray.toString();
+    eliminarEstudiante = function (posicionArray) {
+        id = posicionArray;
         $.ajax({
-            url: `http://localhost:8000/estudiantes${id}`,
+            url: `http://localhost:8000/estudiantes/${id}`,
             method: 'delete'
         }).done(response => {
             const dataJson = JSON.parse(response);
             const msg = dataJson.data;
             alert(msg);
-            reload();
+        
         });
     }
 
--- js/registrarEstudiante.js
+++ js/registrarEstudiante.js
@@ -1,21 +1,21 @@
-$('#guardar').click(function () {
-    alert('hoola');
-        alert('hoola');
-        $codigo = $('#codigo').val();
-        $nombres = $('#nombres').val();
-        $apellidos = $('#apellidos').val();
-        $.ajax({
-            url: 'http://localhost:8000/estudiantes',
-            method: 'post',
-            data: {
-                codigo: $codigo,
-                nombres: $nombres,
-                apellidos: $apellidos
-            }
-        }).done(response => {
-            const dataJson = JSON.parse(response);
-            const msg = dataJson.data;
-            alert(msg);
-            
-        });
-    })
+/**
+ * Funcianalidad para registrar un usuario
+ */
+$("#guardar").click(function () {
+  $codigo = $("#codigo").val();
+  $nombres = $("#nombres").val();
+  $apellidos = $("#apellidos").val();
+  $.ajax({
+    url: "http://localhost:8000/estudiantes",
+    method: "post",
+    data: {
+      codigo: $codigo,
+      nombres: $nombres,
+      apellidos: $apellidos,
+    },
+  }).done((response) => {
+    const dataJson = JSON.parse(response);
+    const msg = dataJson.data;
+    alert(msg);
+  });
+});
